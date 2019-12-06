fab2b857ca11beddec770f91b8d7099849ff6084
Chross <matia.medinag@alumnos.duoc.cl>
2019-12-06 18:33:54 -0300

    Prepare for deploy to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv').config;
-
+const path = require('path');
 dotenv({ path: './config/config.env' });
 
 const app = express();
@@ -16,6 +16,13 @@ app.use('/api/food-menu', require('./routes/foodMenu'));
 app.use('/api/food-order', require('./routes/foodOrder'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
+
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000;
 
